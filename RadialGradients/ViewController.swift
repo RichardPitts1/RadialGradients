9abdee93fadd2034039ffde47e6278f69fd56f87
Richard E Pitts <RichardRPitts@Richard-E-Pittss-Mac.local>
2017-03-10 08:51:08 -0600

    Initial Commit

---
+++ RadialGradients/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RadialGradients
+//
+//  Created by Richard E Pitts on 3/10/17.
+//  Copyright © 2017 Richard E Pitts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
